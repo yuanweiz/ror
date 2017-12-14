67ee9411c84543bc23842a8fcfd3e030882332f6
Weizhe Yuan <yuanweiz1113@gmail.com>
2017-12-14 02:09:17 +0000

    basic styles

--- ror/db/migrate/20171212225612_search_view.rb
+++ ror/db/migrate/20171212225612_search_view.rb
@@ -1,12 +1,15 @@
 class SearchView < ActiveRecord::Migration[5.0]
   def up
-      execute 
-      'CREATE VIEW searchview AS 
+      execute <<-SQL
+      CREATE VIEW searchview AS 
         SELECT "artist"."aname" AS "aname", 
+                "artist"."aid" AS "aid", 
+                "track"."tid" AS "tid", 
                 "artist"."adesc" AS "adesc", 
                 "track"."ttitle" AS "ttitle", 
                 "track"."tgenre" AS "tgenre" 
-        FROM "artist" NATURAL JOIN "track";'
+        FROM "artist" NATURAL JOIN "track";
+	SQL
   end
   def down
       execute 'DROP VIEW searchview;'
